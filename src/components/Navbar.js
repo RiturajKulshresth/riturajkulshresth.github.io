023c1e18a6987e8abdb1b66f06345eb037a4d6c9
Rituraj Kulshresth <idohaveemailid@gmail.com>
2024-03-22 04:55:55 +0530

    added photography

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,6 +89,14 @@ export default function Navbar() {
                 Contact
               </a>
             </li>
+            <li>
+              <a
+                href="https://riturajkulshresth-photography.vercel.app/"
+                className="font-bold block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-600 md:p-0 md:dark:hover:text-green-600 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Photography
+              </a>
+            </li>
           </ul>
         </div>
       </div>
